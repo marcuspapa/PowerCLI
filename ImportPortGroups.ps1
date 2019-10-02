469d3b840ac32b5ec900009eb4b425d231abdf80
Marcus Papa Vieira <marcuspapa@users.noreply.github.com>
2019-10-02 19:58:05 -0300

    Update ImportPortGroups.ps1

--- ImportPortGroups.ps1
+++ ImportPortGroups.ps1
@@ -1,7 +1,19 @@
 ﻿
 
-Get-ChildItem “C:\Users\marcus.papa\Desktop\Marcus\Exports\VCD_PRD” | Foreach {
+$vCenter=” “
+$vCenterUser=” “
+$vCenterUserPassword=” “   #clear text!!!
 
-New-VDPortgroup -VDSwitch Unicloud-vDS_PRODUCAO -Name “$($_.BaseName)” -BackupPath $_.FullName
+write-host “Connecting to vCenter Server $vCenter” -foreground green
+
+Connect-viserver $vCenter -user $vCenterUser -password $vCenterUserPassword -WarningAction 0
+
+#########################################################################################################################################
+
+$vDS = "vDS_PRODUCAO"
+
+Get-ChildItem “C:\Users\path_where_are_the_port_groups_imported” | Foreach {
+
+New-VDPortgroup -VDSwitch $vDS -Name “$($_.BaseName)” -BackupPath $_.FullName
 
 }
