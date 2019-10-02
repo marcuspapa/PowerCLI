48873397d44a471a3fec63b817d180f91dc56e94
Marcus Papa Vieira <marcuspapa@users.noreply.github.com>
2019-10-02 19:52:13 -0300

    Add files via upload

---
+++ ImportPortGroups.ps1
@@ -0,0 +1,7 @@
+﻿
+
+Get-ChildItem “C:\Users\marcus.papa\Desktop\Marcus\Exports\VCD_PRD” | Foreach {
+
+New-VDPortgroup -VDSwitch Unicloud-vDS_PRODUCAO -Name “$($_.BaseName)” -BackupPath $_.FullName
+
+}
