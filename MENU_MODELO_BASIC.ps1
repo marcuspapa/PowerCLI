2d63cd29eb85149b5acba0bb68c11a1d8d3ed577
Marcus Papa Vieira <marcuspapa@users.noreply.github.com>
2024-03-22 00:33:40 -0300

    Add files via upload

---
+++ MENU_MODELO_BASIC.ps1
@@ -0,0 +1,57 @@
+﻿
+
+function Show-Menu
+{
+     param (
+           [string]$Title = ‘My Menu’
+     )
+     cls
+     Write-Host “================ $Title ================”
+    
+     Write-Host “1: Press ‘1’ for this option.”
+     Write-Host “2: Press ‘2’ for this option.”
+     Write-Host “3: Press ‘3’ for this option.”
+     Write-Host “Q: Press ‘Q’ to quit.”
+}
+
+
+
+function Teste-Function
+{
+
+Get-Disk | Out-GridView
+
+}
+
+
+
+
+do
+{
+     Show-Menu
+     $input = Read-Host “Please make a selection”
+     switch ($input)
+     {
+           ‘1’ {
+                cls
+                ‘You chose option #1’
+
+                Teste-Function
+
+           } ‘2’ {
+                cls
+                ‘You chose option #2’
+
+
+                Get-Tag
+
+           } ‘3’ {
+                cls
+                ‘You chose option #3’
+           } ‘q’ {
+                return
+           }
+     }
+     pause
+}
+until ($input -eq ‘q’)
